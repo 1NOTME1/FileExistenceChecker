7a704c251aa5b02968f0f6c1bff76a88cc9904bd
Michał Kwaśniewski <92835884+1NOTME1@users.noreply.github.com>
2023-09-29 14:52:42 +0200

    Update file_existence_checker.py
    new version

--- file_existence_checker.py
+++ file_existence_checker.py
@@ -1,15 +1,23 @@
-#SPRAWDZANIE CZY PLIK ISTNIEJE W DANEJ LOKALIZACJI Z PLIKU .TXT
 import os
 
-def check_files_from_file(file_path):
-    with open(file_path, "r") as f:
-        for line in f:
-            path = line.strip()
-            if os.path.isfile(path):
-                print(f"Plik {path} istnieje w danej lokalizacji!")
-            else:
-                print(f"Plik {path} nie istnieje w danej lokalizacji!")
+def sprawdz_sciezki(nazwa_pliku_txt, wynikowy_plik_txt="nieistniejace_sciezki.txt"):
+    with open(nazwa_pliku_txt, 'r', encoding='utf-8') as plik:
+        sciezki = plik.readlines()
 
-file_path = r"C:\Users\USER_NAME\Desktop\file_existence_checker\pdf_names.txt"
+    nieistniejace = []
 
-check_files_from_file(file_path)
+    for sciezka in sciezki:
+        sciezka = sciezka.strip()  
+        if os.path.exists(sciezka):
+            print(f"Ścieżka istnieje: {sciezka}")
+        else:
+            print(f"Ścieżka NIE istnieje: {sciezka}")
+            nieistniejace.append(sciezka)
+
+    with open(wynikowy_plik_txt, 'w', encoding='utf-8') as plik_wynikowy:
+        for sciezka in nieistniejace:
+            plik_wynikowy.write(sciezka + "\n")
+
+nazwa_pliku = "sciezki.txt"
+sprawdz_sciezki(nazwa_pliku)
+
